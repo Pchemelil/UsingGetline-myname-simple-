1e31817536c78db01da94a1b879b3f56511b4738
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-10-26 20:47:05 +0300

    Add files via upload

---
+++ usingGetline(myname simple).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	string myname;
+	cout << "input your name:\n";
+	getline(cin, myname);
+	cout <<"\n\n";
+	cout << " stop being mean " << myname;
+	return 0;
+	
+	}
